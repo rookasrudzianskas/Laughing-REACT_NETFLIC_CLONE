dc371bd8d2980eb87d78012573714d8eda9b4ccd
Rokas <rookas.rudzenskas@gmail.com>
2021-04-27 07:08:15 +0300

    Rokas

--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
-const Row = () => {
+const Row = ({ title }) => {
+    const [movies, setMovies] = useState([]);
+
+
+
     return (
         <div>
-            Hello
+
+            {/* Rendering rows */}
+
+            <h2>{title}</h2>
+
+
+
+        {/*    container -? posters     */}
         </div>
     )
 }
