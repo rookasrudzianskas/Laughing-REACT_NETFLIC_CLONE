bc7c8aa28def343c1eb609d9546081de3529da37
Rokas <rookas.rudzenskas@gmail.com>
2021-04-27 10:58:33 +0300

    Rokas

--- src/Row.js
+++ src/Row.js
@@ -1,41 +1,66 @@
-import React, {useEffect, useState} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
+import Youtube from "react-youtube";
 
-const base_url = "https://image.tmdb.org/t/p/original/";
+const baseImgUrl = "https://image.tmdb.org/t/p/original";
 
-const Row = ({ title, fetchUrl, isLargeRow }) => {
+function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
-    // console.log(movies)
+    const [trailerUrl, setTrailerUrl] = useState("");
 
-    // a sniper of code which runs on a specific condition
+    // Options for react-youtube
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            autoplay: 1,
+        },
+    };
+
     useEffect(() => {
-        // if [], run once when the row loads, and do not run again
         async function fetchData() {
             const request = await axios.get(fetchUrl);
             setMovies(request.data.results);
             return request;
-            // https://api.themoviedb.org/3 + 🔝
         }
         fetchData();
-    }, [fetchUrl])
+    }, [fetchUrl]);
+
+    const handleClick = async (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl("");
+        } else {
+            let trailerurl = await axios.get(
+                `/movie/${movie.id}/videos?api_key=fb34530271b349314af0de263d16ab5a`
+            );
+            setTrailerUrl(trailerurl.data.results[0]?.key);
+        }
+    };
 
     return (
-             // Rendering rows
-            <div className="row">
-                <h2>{title}</h2>
-                    <div className="row__posters">
-                        {/*    row__poster  */}
-                        {movies.map(movie => (
-                            // "/fdsfsdfafsddfsdfdsfsdfdfsd.jpg //
-                            <img key={movie.id} className={`row__poster ${isLargeRow && "row__posterLarge"}`} src={`${base_url}${isLargeRow ?  movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
-                        ))}
+        <div className="row">
+            <h2>{title}</h2>
 
-
-                </div>
+            <div className="row__posters">
+                {movies.map(
+                    (movie) =>
+                        movie.backdrop_path !== null && (
+                            <img
+                                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                                src={`${baseImgUrl}${
+                                    isLargeRow ? movie.poster_path : movie.backdrop_path
+                                }`}
+                                alt={movie.name}
+                                key={movie.id}
+                                onClick={() => handleClick(movie)}
+                            />
+                        )
+                )}
             </div>
-
-    )
+            {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
+        </div>
+    );
 }
 
 export default Row;
