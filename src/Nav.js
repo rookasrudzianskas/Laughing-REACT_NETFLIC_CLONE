269963b06187045104f6c7bfa945648554cf6cdb
Rokas <rookas.rudzenskas@gmail.com>
2021-04-27 10:26:42 +0300

    Rokas

--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./Nav.css";
 
+
+
 const Nav = () => {
+
+    const [show, handleShow] = useState(false);
+
+    // navbar listener to listen to the scroll to do something
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if(window.scrollY > 100) {
+                handleShow(true);
+            } else {
+                handleShow(false);
+            }
+            return () => {
+                window.removeEventListener("scroll");
+            };
+        });
+    }, [])
+
+
     return (
         <div>
-            <div className="nav">
+            <div className={`nav ${show && "nav__black"}`}>
                 <img
                     className="nav__logo"
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
