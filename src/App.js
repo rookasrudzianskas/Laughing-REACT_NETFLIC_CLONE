f26f65f98f9a56bc67bdb1e195e18cde1b40b758
Rokas <rookas.rudzenskas@gmail.com>
2021-04-27 10:13:38 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import './App.css';
 import Row from "./Row";
 import requests from "./requests";
 import Banner from "./Banner";
-
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="App">
+
+        <Nav />
+
         {/* Navbar  */}
 
         <Banner />
+
         {/* Banner */}
 
         {/* Rows of films */}
